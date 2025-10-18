176e6cdbf06b34d915cb58a5d0306f3651187213
Grau-M <graum@sheridancollege.ca>
2025-10-18 13:10:02 -0400

    Initial Project Generation

---
+++ src/main/java/ca/sheridan/byteme/byteme/BytemeApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridan.byteme.byteme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/sheridan/byteme/byteme/BytemeApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridan.byteme.byteme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
